ef84238913f9df99529eaf115a8bad979a9e3bbd
lry127 <yelei761976@gmail.com>
2026-07-23 11:28:35 +0800

    stream data endpoint impl

--- src/server/proxy_manager.rs
+++ src/server/proxy_manager.rs
@@ -1,6 +1,8 @@
-use bytes::Bytes;
+use bytes::{Bytes, BytesMut};
 use std::future::Future;
 use thiserror::Error;
+use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt, ReadHalf, WriteHalf};
+use tokio::net::{TcpStream, ToSocketAddrs};
 use tokio_util::sync::CancellationToken;
 use tracing::{debug, instrument, warn};
 
@@ -31,13 +33,69 @@ pub trait DataWriter {
     fn shutdown(&mut self) -> impl Future<Output = Result<(), DataEndpointError>> + Send + '_;
 }
 
+struct StreamReader<T> {
+    stream: T,
+    buf: BytesMut,
+}
+
+impl<T: AsyncRead + Unpin + Send> DataReader for StreamReader<T> {
+    async fn read_data(&mut self) -> Result<Option<Bytes>, DataEndpointError> {
+        match self.stream.read_buf(&mut self.buf).await {
+            Ok(n) => {
+                if n == 0 {
+                    Ok(None)
+                } else {
+                    Ok(Some(self.buf.split().freeze()))
+                }
+            }
+            Err(err) => Err(DataEndpointError::IoError(err)),
+        }
+    }
+}
+
+struct StreamWriter<T> {
+    stream: T,
+}
+
+impl<T: AsyncWrite + Unpin + Send> DataWriter for StreamWriter<T> {
+    async fn write_data(&mut self, data: Bytes) -> Result<(), DataEndpointError> {
+        self.stream.write_all(&data).await?;
+        Ok(())
+    }
+
+    async fn shutdown(&mut self) -> Result<(), DataEndpointError> {
+        self.stream.shutdown().await?;
+        Ok(())
+    }
+}
+
+struct StreamDataEndpoint<T> {
+    stream: T,
+    read_buf_len: usize,
+}
+
+impl<T: AsyncRead + AsyncWrite + Send + Unpin + 'static> DataEndpoint for StreamDataEndpoint<T> {
+    type ReadHalf = StreamReader<ReadHalf<T>>;
+    type WriteHalf = StreamWriter<WriteHalf<T>>;
+
+    fn split(self) -> (Self::ReadHalf, Self::WriteHalf) {
+        let (read_half, write_half) = tokio::io::split(self.stream);
+        let reader = StreamReader {
+            stream: read_half,
+            buf: BytesMut::with_capacity(self.read_buf_len),
+        };
+        let writer = StreamWriter { stream: write_half };
+        (reader, writer)
+    }
+}
+
 struct BidirectionalCopier<A, B>
 where
     A: DataEndpoint,
     B: DataEndpoint,
 {
-    endpoint_alice: A,
-    endpoint_bob: B,
+    endpoint_a: A,
+    endpoint_b: B,
 }
 
 impl<A, B> BidirectionalCopier<A, B>
@@ -46,8 +104,8 @@ where
     B: DataEndpoint,
 {
     fn spawn_copy_tasks(self, cancellation_token: CancellationToken) {
-        let (rx_a, tx_a) = self.endpoint_alice.split();
-        let (rx_b, tx_b) = self.endpoint_bob.split();
+        let (rx_a, tx_a) = self.endpoint_a.split();
+        let (rx_b, tx_b) = self.endpoint_b.split();
 
         // Clone the token for each direction
         let token_a = cancellation_token;
@@ -104,17 +162,47 @@ where
 }
 
 /// Manages a group of proxy connections
-pub struct ProxyManager {}
+pub struct ProxyManager {
+    root_cancellation_token: CancellationToken,
+    read_chunk_size: usize,
+}
 
 impl ProxyManager {
-    async fn start_session<A: DataEndpoint, B: DataEndpoint>(endpoint_a: A, endpoint_b: B) {
-        let token = CancellationToken::new();
+    pub fn new(read_chunk_size: usize) -> Self {
+        Self {
+            root_cancellation_token: CancellationToken::new(),
+            read_chunk_size,
+        }
+    }
+
+    pub fn default() -> Self {
+        Self::new(8192)
+    }
+
+    pub async fn tcp_connect_to_target<T: ToSocketAddrs>(
+        &self,
+        remote: T,
+    ) -> tokio::io::Result<impl DataEndpoint> {
+        let tcp_stream = TcpStream::connect(remote).await?;
+        let stream_endpoint = StreamDataEndpoint {
+            stream: tcp_stream,
+            read_buf_len: self.read_chunk_size,
+        };
+        Ok(stream_endpoint)
+    }
+
+    pub fn start_session<A: DataEndpoint, B: DataEndpoint>(&self, endpoint_a: A, endpoint_b: B) {
+        let token = self.root_cancellation_token.child_token();
 
         let copier = BidirectionalCopier {
-            endpoint_alice: endpoint_a,
-            endpoint_bob: endpoint_b,
+            endpoint_a: endpoint_a,
+            endpoint_b: endpoint_b,
         };
 
         copier.spawn_copy_tasks(token);
+    }
+
+    pub fn shutdown_manager(&self) {
+        self.root_cancellation_token.cancel();
     }
 }
